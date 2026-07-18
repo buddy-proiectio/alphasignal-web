c2588688287d40ca33beb873396e43030b9b615e
taevel02 <taevel02@gmail.com>
2026-07-18 12:00:06 +1000

    Revert "chore: remove unused Analytics component"
    This reverts commit ae24cdbbb88eb44fde11648e4e75832b86fbd704.

---
+++ src/components/Analytics.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React from "react";
+import Script from "next/script";
+
+interface AnalyticsProps {
+  gaId?: string;
+}
+
+export default function Analytics({ gaId = "G-XXXXXXXXXX" }: AnalyticsProps) {
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${gaId}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+    </>
+  );
+}
